fe936dbe523b1323bcac896e1181c1b3e6bac461
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-04-26 16:45:17 +1000

    create canvas

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hi')
+var canvas = document.getElementById("board");
+var ctx = canvas.getContext("2d");
+
+var width = 10;
+var height = 20;
+var pixels = 24;
+
+canvas.width = width*pixels
+canvas.height = height*pixels
+
+
+
