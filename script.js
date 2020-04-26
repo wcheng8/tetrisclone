b2d479a2dad85502aab25a852bc455d07060ed64
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-04-26 17:24:27 +1000

    draw board

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ var ctx = canvas.getContext("2d");
 var width = 10;
 var height = 20;
 var pixels = 24;
+var state = true;
 
 canvas.width = width*pixels
 canvas.height = height*pixels
 
+// Draw grey board
+function drawboard(){
+  ctx.strokeStyle = "#555"
+  for(let y = 0;y<height;y++){
+    for(let x = 0; x<height;x++){
+      ctx.strokeRect(x*pixels,y*pixels,pixels,pixels)
+    }
+  }
+}
+drawboard()
 
 
