93bd633940fef7c3802722863b559271ca5e1d59
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-04-28 22:52:04 +1000

    rotate tetrimino, set boundary

--- script.js
+++ script.js
@@ -73,6 +73,36 @@ function playerMove(dir){
   }
 }
 
+// Player rotate
+function playerRotate(dir){
+  const pos = player.pos.x
+  let offset = 1
+  rotate(player.matrix)
+  // check collision in while
+  while (collide(arena,player)){
+    player.pos.x += offset
+    offset = -(offset+(offset>0?1:-1))
+    if (offset>player.matrix[0].length){
+      rotate(player,-dir)
+      player.pos.x = pos
+      return
+    }
+  }
+}
+// Rotate function
+function rotate(matrix,dir){
+  for (let y = 0; y<matrix.length; y++){
+    for (let x = 0; x<y;x++){
+      [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]]
+    }
+  }
+  if (dir > 0){
+    matrix.forEach(row => row.reverse())
+  }else{
+    matrix.reverse()
+  }
+}
+
 // Add droping factor to the peice
 let dropCounter = 0;
 let dropInterval = 1000;
@@ -121,6 +151,8 @@ document.addEventListener("keydown", (event) => {
     playerMove(1)
   } else if (event.keyCode == 40) {
     playerDrop();
+  }else if(event.keyCode == 38){
+    playerRotate(1)
   }
 });
 
