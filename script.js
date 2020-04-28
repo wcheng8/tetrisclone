cc7abfac6a894222b9b27d80c8b68d27382ee49c
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-04-28 23:39:42 +1000

    add colors and randomized tetriminoes

--- script.js
+++ script.js
@@ -16,13 +16,65 @@ function createMatrix(w, h) {
   }
   return matrix;
 }
+//  Create matrix peice
+function createPeice(type) {
+  if (type == "T") {
+    return [
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ];
+  } else if (type == "O") {
+    return [
+      [2, 2],
+      [2, 2],
+    ];
+  } else if (type == "L") {
+    return [
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3],
+    ];
+  } else if (type == "J") {
+    return [
+      [0, 4, 0],
+      [0, 4, 0],
+      [4, 4, 0],
+    ];
+  } else if (type == "I") {
+    return [
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+    ];
+  } else if (type == "Z") {
+    return [
+      [6, 6, 0],
+      [0, 6, 6],
+      [0, 0, 0],
+    ];
+  } else if (type == "S") {
+    return [
+      [0, 7, 7],
+      [7, 7, 0],
+      [0, 0, 0],
+    ];
+  }
+}
 
-// Tetrimono
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0],
-];
+function playerReset() {
+  const peices = "ILJOTSZ";
+  player.matrix = createPeice(
+    peices[Math.floor(peices.length * Math.random())]
+  );
+  player.pos.x = 5;
+  player.pos.y = 0;
+  if (collide(arena, player)) {
+    alert("game over");
+    arena.forEach((row) => row.fill(0));
+  }
+}
 
 // Detect if collision exists
 function collide(arena, player) {
@@ -40,15 +92,25 @@ function collide(arena, player) {
 // Draw tetrimino function
 function draw() {
   clearcanvas();
-  drawMatrix(arena, {x:0, y:0})
+  drawMatrix(arena, { x: 0, y: 0 });
   drawMatrix(player.matrix, player.pos);
 }
 
 function drawMatrix(matrix, offset) {
+  const colors = [
+    null,
+    "red",
+    "blue",
+    "yellow",
+    "green",
+    "orange",
+    "purple",
+    "aqua",
+  ];  
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = "red";
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
       }
     });
@@ -61,45 +123,46 @@ function playerDrop() {
   if (collide(arena, player)) {
     player.pos.y--;
     merge(arena, player);
+    playerReset();
     player.pos.y = 0;
   }
   dropCounter = 0;
 }
 
-function playerMove(dir){
-  player.pos.x+= dir
-  if (collide(arena,player)){
-    player.pos.x -=dir
+function playerMove(dir) {
+  player.pos.x += dir;
+  if (collide(arena, player)) {
+    player.pos.x -= dir;
   }
 }
 
 // Player rotate
-function playerRotate(dir){
-  const pos = player.pos.x
-  let offset = 1
-  rotate(player.matrix)
+function playerRotate(dir) {
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix);
   // check collision in while
-  while (collide(arena,player)){
-    player.pos.x += offset
-    offset = -(offset+(offset>0?1:-1))
-    if (offset>player.matrix[0].length){
-      rotate(player,-dir)
-      player.pos.x = pos
-      return
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.matrix[0].length) {
+      rotate(player, -dir);
+      player.pos.x = pos;
+      return;
     }
   }
 }
 // Rotate function
-function rotate(matrix,dir){
-  for (let y = 0; y<matrix.length; y++){
-    for (let x = 0; x<y;x++){
-      [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]]
+function rotate(matrix, dir) {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < y; x++) {
+      [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
     }
   }
-  if (dir > 0){
-    matrix.forEach(row => row.reverse())
-  }else{
-    matrix.reverse()
+  if (dir > 0) {
+    matrix.forEach((row) => row.reverse());
+  } else {
+    matrix.reverse();
   }
 }
 
@@ -125,7 +188,7 @@ function update(time = 0) {
 // Current player position
 const player = {
   pos: { x: 5, y: 5 },
-  matrix: matrix,
+  matrix: createPeice("L"),
 };
 
 // Update tetrimino to the arena board
@@ -146,13 +209,13 @@ console.table(arena);
 // Arrow key handling
 document.addEventListener("keydown", (event) => {
   if (event.keyCode == 37) {
-    playerMove(-1)
+    playerMove(-1);
   } else if (event.keyCode == 39) {
-    playerMove(1)
+    playerMove(1);
   } else if (event.keyCode == 40) {
     playerDrop();
-  }else if(event.keyCode == 38){
-    playerRotate(1)
+  } else if (event.keyCode == 38) {
+    playerRotate(1);
   }
 });
 
