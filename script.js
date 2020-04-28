1fc5263f9c507459cfeb7f11d497c0ef9d8b56f3
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-04-28 18:57:53 +1000

    add arrow function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("board");
 const context = canvas.getContext("2d");
-
+let play = true
 context.scale(20, 20);
 
 function clearcanvas() {
@@ -29,7 +29,10 @@ function drawMatrix(matrix, offset) {
     });
   });
 }
-
+function playerDrop() {
+  player.pos.y++;
+  dropCounter = 0;
+}
 // Add droping factor to the peice
 let dropCounter = 0;
 let dropInterval = 1000;
@@ -40,17 +43,28 @@ function update(time = 0) {
   lastTime = time;
   dropCounter += deltaTime;
   if (dropCounter > dropInterval) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerDrop();
   }
   lasttime = time;
   draw();
-  requestAnimationFrame(update);
+  if (play){
+    requestAnimationFrame(update);
+  }
 }
 
 const player = {
   pos: { x: 5, y: 5 },
   matrix: matrix,
 };
+
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode == 37) {
+    player.pos.x--;
+  } else if (event.keyCode == 39) {
+    player.pos.x++;
+  } else if (event.keyCode == 40) {
+    playerDrop();
+  }
+});
 
 update();
