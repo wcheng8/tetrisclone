28fba1d69398604d5030570f2c82dafc9ac305d9
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-04-28 23:57:20 +1000

    clearing rows

--- script.js
+++ script.js
@@ -63,6 +63,19 @@ function createPeice(type) {
   }
 }
 
+function arenaSweep(){
+  outer: for (let y = arena.length -1; y>0; y--){
+    for (let x = 0;x<arena[y].length;x++){
+      if (arena[y][x] == 0){
+        continue outer;
+      }
+    }
+    const row = arena.splice(y,1)[0].fill(0)
+    arena.unshift(row)
+    y++
+  }
+}
+
 function playerReset() {
   const peices = "ILJOTSZ";
   player.matrix = createPeice(
@@ -124,7 +137,7 @@ function playerDrop() {
     player.pos.y--;
     merge(arena, player);
     playerReset();
-    player.pos.y = 0;
+    arenaSweep();
   }
   dropCounter = 0;
 }
