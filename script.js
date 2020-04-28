0a9518c69934629ad45ce1eaed2e2d06cb539cab
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-04-28 19:34:06 +1000

    check if there is collision create arena that merges tiles on to the game board

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("board");
 const context = canvas.getContext("2d");
-let play = true
+let play = true;
 context.scale(20, 20);
 
 function clearcanvas() {
@@ -8,12 +8,36 @@ function clearcanvas() {
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+// Create current board matrix
+function createMatrix(w, h) {
+  const matrix = [];
+  for (let y = 0; y < h; y++) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
+// Tetrimono
 const matrix = [
   [0, 0, 0],
   [1, 1, 1],
   [0, 1, 0],
 ];
 
+// Detect if collision exists
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// Draw tetrimino function
 function draw() {
   clearcanvas();
   drawMatrix(player.matrix, player.pos);
@@ -29,8 +53,15 @@ function drawMatrix(matrix, offset) {
     });
   });
 }
+
+// Drop tetrimino
 function playerDrop() {
   player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
   dropCounter = 0;
 }
 // Add droping factor to the peice
@@ -47,16 +78,33 @@ function update(time = 0) {
   }
   lasttime = time;
   draw();
-  if (play){
+  if (play) {
     requestAnimationFrame(update);
   }
 }
 
+// Current player position
 const player = {
   pos: { x: 5, y: 5 },
   matrix: matrix,
 };
 
+// Update tetrimino to the arena board
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
+// Board Matrix
+const arena = createMatrix(12, 20);
+console.table(arena);
+
+// Arrow key handling
 document.addEventListener("keydown", (event) => {
   if (event.keyCode == 37) {
     player.pos.x--;
