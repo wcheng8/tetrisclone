0fc03b709957e6838422638fa7cf2e2dfbbda83a
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-04-28 17:33:09 +1000

    add drop counter

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
-var canvas = document.getElementById("board");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("board");
+const context = canvas.getContext("2d");
 
-var width = 10;
-var height = 20;
-var pixels = 24;
-var state = true;
+context.scale(20, 20);
 
-canvas.width = width*pixels
-canvas.height = height*pixels
-
-// Draw grey board
-function drawboard(){
-  ctx.strokeStyle = "#555"
-  for(let y = 0;y<height;y++){
-    for(let x = 0; x<height;x++){
-      ctx.strokeRect(x*pixels,y*pixels,pixels,pixels)
-    }
-  }
+function clearcanvas() {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 }
-drawboard()
 
+const matrix = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 1, 0],
+];
 
+function draw() {
+  clearcanvas();
+  drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle = "red";
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    });
+  });
+}
+
+// Add droping factor to the peice
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    player.pos.y++;
+    dropCounter = 0;
+  }
+  lasttime = time;
+  draw();
+  requestAnimationFrame(update);
+}
+
+const player = {
+  pos: { x: 5, y: 5 },
+  matrix: matrix,
+};
+
+update();
